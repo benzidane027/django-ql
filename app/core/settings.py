0828a6f53390cb7002b11a3ddfce7c3f334e5557
benzidane027 <benzidan27@yahoo.com>
2024-10-03 10:40:03 +0100

    fix media folder

--- app/core/settings.py
+++ app/core/settings.py
@@ -18,6 +18,8 @@ from decouple import config as ENV
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 #AUTH_PROFILE_MODULE = 'accounts.UserProfile'
 AUTH_USER_MODEL = "accounts.profile"
 
@@ -193,7 +195,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
