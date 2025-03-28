f103c2360df2b7389a17a2e007cabe5e11302893
benzidane027 <benzidan27@yahoo.com>
2025-03-28 17:21:14 +0100

    fix

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -18,10 +18,11 @@ urlpatterns = [
 # /users/set_username/
 # /users/reset_username/
 # /users/reset_username_confirm/
+
 # /token/login/ (Token Based Authentication)
 # /token/logout/ (Token Based Authentication)
 
-            #### not used ####
+            #### used ####
 # /jwt/create/ (JSON Web Token Authentication)
 # /jwt/refresh/ (JSON Web Token Authentication)
 # /jwt/verify/ (JSON Web Token Authentication)
